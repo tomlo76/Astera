026a0b95b30f6824b7c15f6964d02da55ce8e28e
thomas_l <thomas.lods@epfl.ch>
2021-05-18 15:59:44 +0200

    Add possibility to change teleportation circle rotation speed

--- Assets/Scripts/Teleportation/TeleportationPoint.cs
+++ Assets/Scripts/Teleportation/TeleportationPoint.cs
@@ -6,6 +6,7 @@ public class TeleportationPoint : MonoBehaviour
 {
     [Header("Properties")]
     public bool rotateOnActive = false;
+    public float speed = 25;
 
     [Header("Default values")]
     public bool active = false;
@@ -23,9 +24,9 @@ public class TeleportationPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Model from blender, so right hand convention is used, Vector3.forward = (0, 0, 1)
+        // Model from blender, so right hand convention is used, Vector3.forward = (0, 0, 1) is the up vector
         // Doc : https://docs.unity3d.com/ScriptReference/Vector3.html
-        if (rotateOnActive && active) transform.Rotate(Vector3.forward * 50 * Time.deltaTime, Space.Self);
+        if (rotateOnActive && active) transform.Rotate(Vector3.forward * speed * Time.deltaTime, Space.Self);
     }
 
     public void activate(bool a = true)
